0f811bdd7254ad3b2bfd8b5e6cc11bcf67e10621
MTayeb93 <m.tayeb993@gmail.com>
2023-05-27 13:25:27 +0300

    set up api routes

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,24 +3,26 @@ import Nav from '@components/Nav'
 import Provider from '@components/Provider'
 
 export const metadata = {
- title: 'Promptopia',
- description: 'Discover & Share AI Prompts'
+  title: 'Promptopia',
+  description: 'Discover & Share AI Prompts',
 }
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang='en'>
       <body>
-       <div>
-        <div className="main">
-         <div className='gradient' />
-        </div>
-       </div>
+        <Provider>
+          <div>
+            <div className='main'>
+              <div className='gradient' />
+            </div>
+          </div>
 
-       <main className='app'>
-        <Nav />
-        {children}
-       </main>
+          <main className='app'>
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   )
